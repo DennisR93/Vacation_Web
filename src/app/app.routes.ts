40da6e31b005a362b741b7540d4fd10cd3fe63b1
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 12:43:36 +0200

    changing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {AppLayoutComponent} from './pages/app-layout/app-layout.component';
+import {RegisterComponent} from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children : [
+      {
+        path: 'register', component: RegisterComponent
+      }
+    ]
+  }
+];
