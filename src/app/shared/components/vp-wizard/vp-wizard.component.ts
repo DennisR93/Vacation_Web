e60618515b33487a5b7ab41b5284aace2a3a0025
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 13:46:58 +0200

    Creating Wizard

---
+++ src/app/shared/components/vp-wizard/vp-wizard.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vp-wizard',
+  imports: [],
+  templateUrl: './vp-wizard.component.html',
+  styleUrl: './vp-wizard.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class VpWizardComponent {
+
+}
