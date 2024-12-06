0a519ada743a8f65c546ec082aa92cd5dd804231
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 18:39:52 +0200

    Adding methods to Wizard

--- src/app/shared/components/vp-wizard/vp-wizard.component.ts
+++ src/app/shared/components/vp-wizard/vp-wizard.component.ts
@@ -1,12 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {RegisterComponent} from '../../../pages/register/register.component';
+import {HomeComponent} from '../../../pages/home/home.component';
+import {VpWizardStepComponent} from './vp-wizard-step/vp-wizard-step.component';
 
 @Component({
   selector: 'app-vp-wizard',
-  imports: [],
+  imports: [RegisterComponent, HomeComponent, VpWizardStepComponent],
   templateUrl: './vp-wizard.component.html',
   styleUrl: './vp-wizard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VpWizardComponent {
+  @Input() steps:any[] = [];
+  @Input() wizardTitle!: string;
+
+  protected currentStep:number = 0;
+
+
+  //TODO - Refactor to handle from inner component
+  goNext() {
+    if (this.currentStep < this.steps.length - 1) {
+      this.currentStep++;
+    }
+  }
+
+  goBack() {
+    if (this.currentStep > 0) {
+      this.currentStep--;
+    }
+  }
+
+  isLastStep() {
+    return this.currentStep === this.steps.length - 1;
+  }
+
+  finish() {
+    console.log('Wizard Completed!');
+  }
 
 }
