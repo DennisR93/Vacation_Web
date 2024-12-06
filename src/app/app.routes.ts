2605fc381f89c5cee7f193286379b8a08a6acb42
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 12:50:38 +0200

    Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 import {AppLayoutComponent} from './pages/app-layout/app-layout.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {HomeComponent} from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: AppLayoutComponent,
     children : [
-      {
-        path: 'register', component: RegisterComponent
-      }
+      { path: 'home', component: HomeComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
     ]
   }
 ];
