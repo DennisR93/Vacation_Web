8fdf75e47e387f498d4b7991687c7d764149ffee
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 12:50:17 +0200

    Adding imports

--- src/app/pages/app-layout/app-layout.component.ts
+++ src/app/pages/app-layout/app-layout.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from '../../core/components/header/header.component';
+import {FooterComponent} from '../../core/components/footer/footer.component';
 
 @Component({
   selector: 'app-app-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
