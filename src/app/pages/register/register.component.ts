0edf8d1247e10641c928a4194c327cf153ac7a42
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 18:41:01 +0200

    Starting Form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,37 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {VpWizardComponent} from '../../shared/components/vp-wizard/vp-wizard.component';
+import {FormGroup, NonNullableFormBuilder} from '@angular/forms';
+import {RegistrationFirstStep, RegistrationForm, RegistrationSecondStep} from './models/registration-form-model';
+import {RegisterFirstStepComponent} from './components/register-first-step/register-first-step.component';
+import {RegisterSecondStepComponent} from './components/register-second-step/register-second-step.component';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [VpWizardComponent, RegisterFirstStepComponent, RegisterSecondStepComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterComponent {
+  protected registrationSteps = [{component: RegisterFirstStepComponent}, {component: RegisterSecondStepComponent}];
 
+  fb = inject(NonNullableFormBuilder);
+
+  registrationForm: FormGroup = this.fb.group<RegistrationForm>({
+    firstStep: this.fb.group<RegistrationFirstStep>({
+      id: this.fb.control(''),
+      firstName: this.fb.control(''),
+      lastName: this.fb.control(''),
+      mobilePrefix: this.fb.control(''),
+      mobileNumber: this.fb.control(''),
+      email: this.fb.control('')
+    }),
+    secondStep: this.fb.group<RegistrationSecondStep>({
+      city:this.fb.control(''),
+      street: this.fb.control(''),
+      houseNumber:this.fb.control(''),
+      password: this.fb.control(''),
+      confirmPassword: this.fb.control('')
+    })
+  })
 }
