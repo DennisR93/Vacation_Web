243953d026a88e257d4a83015bfb792ff80c64f4
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 18:36:05 +0200

    Creating registration second step

---
+++ src/app/pages/register/components/register-second-step/register-second-step.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-second-step',
+  imports: [],
+  templateUrl: './register-second-step.component.html',
+  styleUrl: './register-second-step.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterSecondStepComponent {
+
+}
