5dfa3dbf49a94bf9ee9748158b31d283492a36b2
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-06 18:35:59 +0200

    Creating registration first step

---
+++ src/app/pages/register/components/register-first-step/register-first-step.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-first-step',
+  imports: [],
+  templateUrl: './register-first-step.component.html',
+  styleUrl: './register-first-step.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterFirstStepComponent {
+
+}
