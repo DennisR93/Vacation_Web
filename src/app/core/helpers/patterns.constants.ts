d008642be906c0787d1187bafa9b8a390ec333d7
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-07 17:45:56 +0200

    Creating patterns constants

---
+++ src/app/core/helpers/patterns.constants.ts
@@ -0,0 +1,7 @@
+export const patterns = {
+  email: '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,15}$',
+  names: '^[a-zA-Z.\\-\"\']+$',
+  phoneNumber:'^\\d{7}',
+  houseNumber:'\\d{1,3}',
+  caseStatus: '^\\d{6}-\\d{1,15}$'
+}
