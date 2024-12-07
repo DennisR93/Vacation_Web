2bb0dfcb0f9a4889edba698022c55997b29486c2
dennisrabinovitch <rdennis0993@gmail.com>
2024-12-07 18:57:12 +0200

    Creating Second step form

--- src/app/pages/register/components/register-second-step/register-second-step.component.ts
+++ src/app/pages/register/components/register-second-step/register-second-step.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {RegistrationSecondStep} from '../../models/registration-form-model';
+import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register-second-step',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-second-step.component.html',
   styleUrl: './register-second-step.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterSecondStepComponent {
+  @Input() formStep!: FormGroup<RegistrationSecondStep>
 
 }
