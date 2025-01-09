339191e2fa6a79fb99f0d57bdf259bfb26967acb
dennisrabinovitch <rdennis0993@gmail.com>
2025-01-09 21:56:22 +0200

    TS - Work in progress

--- src/app/shared/components/vp-wizard/vp-wizard.component.ts
+++ src/app/shared/components/vp-wizard/vp-wizard.component.ts
@@ -1,34 +1,36 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
-import {RegisterComponent} from '../../../pages/register/register.component';
-import {HomeComponent} from '../../../pages/home/home.component';
+import {ChangeDetectionStrategy, Component, Input, input} from '@angular/core';
 import {VpWizardStepComponent} from './vp-wizard-step/vp-wizard-step.component';
-import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-vp-wizard',
-  imports: [RegisterComponent, HomeComponent, VpWizardStepComponent],
+  imports: [VpWizardStepComponent],
   templateUrl: './vp-wizard.component.html',
   styleUrl: './vp-wizard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VpWizardComponent {
-  @Input() steps:any[] = [];
-  @Input() wizardTitle!: string;
-  @Input() form?: FormGroup;
+  steps = input<any>([]);
+  containerTitle = input<string>('');
 
   protected currentStep:number = 0;
 
 
   //TODO - Refactor to handle from inner component
   goNext() {
-    if (this.currentStep < this.steps.length - 1) {
+    console.log(this.steps().length);
+    console.log('Next button clicked. Current step before:', this.currentStep);
+    if (this.currentStep < this.steps().length - 1) {
       this.currentStep++;
+      console.log('Moved to next step. Current step after:', this.currentStep);
     }
   }
 
+
   goBack() {
+    console.log('Back button clicked. Current step before:', this.currentStep);
     if (this.currentStep > 0) {
       this.currentStep--;
+      console.log('Moved to previous step. Current step after:', this.currentStep);
     }
   }
 
