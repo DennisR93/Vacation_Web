bce7316f598e26eac80b4aea858a796d20013f5d
dennisrabinovitch <rdennis0993@gmail.com>
2025-01-09 21:55:30 +0200

    Adding Const

--- src/app/pages/register/components/register-first-step/register-first-step.component.ts
+++ src/app/pages/register/components/register-first-step/register-first-step.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {RegistrationFirstStep} from '../../models/registration-form-model';
+import {RegisterFormConst} from '../../../../core/consts/registration-consts';
 
 @Component({
   selector: 'app-register-first-step',
@@ -11,5 +12,6 @@ import {RegistrationFirstStep} from '../../models/registration-form-model';
 })
 export class RegisterFirstStepComponent {
   @Input() formStep!: FormGroup<RegistrationFirstStep>;
+  formConst = RegisterFormConst;
 
 }
