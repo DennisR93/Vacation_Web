ff0aaffd95a34e508cbf6b085ca8c5cd0c7ff05a
dennisrabinovitch <rdennis0993@gmail.com>
2025-01-09 21:54:05 +0200

    Importing Accessibility skip link

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {SkipLinkComponent} from '../../../shared/components/skip-link/skip-link.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, SkipLinkComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
